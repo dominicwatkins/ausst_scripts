a722325f0b98b806efe147e380b80ccd3248e41d
dominicwatkins <dwatkins@adobe.com>
2017-05-23 13:06:55 +0100

    Create update_ausst.bat

---
+++ update_ausst.bat
@@ -0,0 +1,13 @@
+@echo off
+REM Script to update currently downloaded Adobe Update Server Setup Tool files used for updating applications
+
+REM Location of update files
+set AUSSTROOT="E:\Installers\AUSST"
+set RUNFROM="C:\Program Files (x86)\Common Files\Adobe\OOBE\PDApp\CCP\utilities\AUSST"
+
+C:
+cd %RUNFROM%
+
+AdobeUpdateServerSetupTool.exe --root="%AUSSTROOT%" --incremental
+
+pause
